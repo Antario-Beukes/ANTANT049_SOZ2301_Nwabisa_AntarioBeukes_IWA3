93ab90d14fbb8da1a739156f6a59172b9aac4d5b
Antario-Beukes <antariobeukes41@gmail.com>
2023-03-29 15:29:20 +0200

    IWA 3

---
+++ Challenge2/scriptsChall2.js
@@ -0,0 +1,7 @@
+// scripts.js
+
+import { role as alexrole } from "./alex.js";
+import { role as johannesrole } from "./johannes.js";
+import { role as nwabisarole } from "./nwabisa.js";
+
+console.log('Roles:', nwabisarole, johannesrole, alexrole);
